26eaa0fb4b6834ce36400f2d4a50dbe8915dbd36
NettixCode <nettixcode@gmail.com>
2024-08-03 01:14:16 +0700

    Update GitHub Action workflow and add debugging

--- copyLaravelSrc.php
+++ copyLaravelSrc.php
@@ -1,15 +1,19 @@
+// copyLaravelSrc.php
 <?php
 
 function recurse_copy($src, $dst) {
     $dir = opendir($src);
     @mkdir($dst);
-    while(false !== ( $file = readdir($dir)) ) {
-        if (( $file != '.' ) && ( $file != '..' )) {
-            if ( is_dir($src . '/' . $file) ) {
-                recurse_copy($src . '/' . $file,$dst . '/' . $file);
-            }
-            else {
-                copy($src . '/' . $file,$dst . '/' . $file);
+    while (false !== ($file = readdir($dir))) {
+        if (($file != '.') && ($file != '..')) {
+            if (is_dir($src . '/' . $file)) {
+                recurse_copy($src . '/' . $file, $dst . '/' . $file);
+            } else {
+                if (copy($src . '/' . $file, $dst . '/' . $file)) {
+                    echo "Copied file $src/$file to $dst/$file\n";
+                } else {
+                    echo "Failed to copy file $src/$file to $dst/$file\n";
+                }
             }
         }
     }
@@ -19,6 +23,15 @@ function recurse_copy($src, $dst) {
 $src = 'laravel-framework/src';
 $dst = 'src';
 
+if (!is_dir($src)) {
+    echo "Source directory $src does not exist.\n";
+    exit(1);
+}
+
 recurse_copy($src, $dst);
 
-echo "Laravel framework source files have been copied to src folder.\n";
+if (is_dir($dst)) {
+    echo "Laravel framework source files have been copied to src folder.\n";
+} else {
+    echo "Failed to copy Laravel framework source files.\n";
+}
